84ba72386d3f03fc4d94120d64bf3c59859f3191
Saida Davis <saihdavis@gmail.com>
2020-04-18 18:51:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,12 @@
 class Song 
   
   attr_accessor :name :artist :genre 
+   
    @@count = 0 
-  def initialize(name, artist, genre)
+   
+  def initialize(name)
+    @@count =+ 1 
+  end
     
    
 
