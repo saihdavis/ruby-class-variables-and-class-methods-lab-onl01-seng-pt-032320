0a710dfec6c8692f7e786ea027bea09bad5c394d
Saida Davis <saihdavis@gmail.com>
2020-04-18 22:46:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,16 +3,16 @@ class Song
   attr_accessor :name, :artist, :genre
    
    @@count = 0 
-   @@artist_all = []
-   @@genre_all = []
+   @@artists_all = []
+   @@genres_all = []
    
   def initialize(name, artist, genre)
     @@count += 1 
     @name = name
     @artist = artist
     @genre = genre
-    @@artist_all << artist 
-    @@genre_all << genre
+    @@artists_all << artist 
+    @@genres_all << genre
   end
    
   def self.count()
